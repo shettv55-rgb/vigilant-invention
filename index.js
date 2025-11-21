bf6e632bcb9f1b2b28c76389ba1f6b2f8ffaa762
Theyllor <theyllorhenrique@gmail.com>
2025-11-20 21:12:09 -0300

    to doido

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(process.cwd(), "frontend")));
 
 // Rota principal abre cursos.html
 app.get("/", (req, res) => {
-  res.sendFile(path.join(process.cwd(), "frontend/cursos.html"));
+  res.sendFile(path.join(process.cwd(), "frontend/curso.html"));
 });
 
 // Criar sessão de pagamento
